08dc0f2c3c1344cb1c7732148bee752a88cf65cb
marsasgrg <92935256+marsasgrg@users.noreply.github.com>
2025-06-25 13:31:39 +0300

    Clickable buttons

--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
-export function setupCounter(element) {
+export function setUpQuestions(element) {
    let counter = 0;
 
-  const adjustCounterValue = value => {
-    if (value >= 100) return value - 100;
-    if (value <= -100) return value + 100;
-    return value;
+  const pressButton = value => {
+    console.log(value);
   };
 
-  const setCounter = value => {
-    counter = adjustCounterValue(value);
-    element.innerHTML = `${counter}`;
-  };
+  document.getElementById('choiceA').addEventListener('click', () => pressButton("A"));
+  document.getElementById('choiceB').addEventListener('click', () => pressButton("B"));
+  document.getElementById('choiceC').addEventListener('click', () => pressButton("C"));
+  document.getElementById('choiceD').addEventListener('click', () => pressButton("D"));
 
-  document.getElementById('increaseByOne').addEventListener('click', () => setCounter(counter + 1));
-  document.getElementById('decreaseByOne').addEventListener('click', () => setCounter(counter - 1));
-  document.getElementById('increaseByTwo').addEventListener('click', () => setCounter(counter + 2));
-  document.getElementById('decreaseByTwo').addEventListener('click', () => setCounter(counter - 2));
 
-    setCounter(0);
 }
 
-setupCounter(document.getElementById('counter-value'));
+setUpQuestions(document.getElementById('counter-value'));
 
